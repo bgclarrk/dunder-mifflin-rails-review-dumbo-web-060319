1ce53d72f52ffd36a9e7727deb76d0f2ecfb6891
+ <+>
2021-06-29 15:06:19 -0700

    Dunder Mifflin pupper application

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,29 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def edit
+        
+    end
+    
+    def create
+
+    end
+
+    private
+
+    def dog_params
+
+    end
+
 end
